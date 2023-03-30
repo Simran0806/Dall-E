eaf318027456bc930cb948dd2a93ba8722a5a776
Simran0806 <127962890+Simran0806@users.noreply.github.com>
2023-03-31 03:03:06 +0530

    Update utilFunctions.js

--- frontend/src/Utils/utilFunctions.js
+++ frontend/src/Utils/utilFunctions.js
@@ -8,7 +8,7 @@ export const generateImage = async (prompt) => {
 
     try {
         const response = await axios.post("http://localhost:8888/generateImage", { prompt })
-        console.log(response.data.url);
+//         console.log(response.data.url);
         return response.data.url
 
     } catch (error) {
@@ -26,16 +26,13 @@ export const editImage = async (img, mask, prompt) => {
             alert("Please enter a valid prompt");
             return;
         }
-        console.log("wait");
-        console.log(img);
-        console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-        console.log(mask.split(",")[1]);
+      
         mask = mask.split(",")[1]
 
 
         const response = await axios.post("http://localhost:8888/edit", { img, mask, prompt });
 
-        console.log(response.data.url);
+//         console.log(response.data.url);
         return response.data.url
 
     } catch (error) {
@@ -48,7 +45,7 @@ export const editImage = async (img, mask, prompt) => {
 export const generateVariations = async (base64_url) => {
     try {
         const response = await axios.post("http://localhost:8888/variations", { base64_url })
-        console.log(response.data.url);
+//         console.log(response.data.url);
         return response.data.url
     } catch (error) {
         console.log(error);
